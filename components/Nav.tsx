c5b7e33ea603fd18fe91b137d77435634780857d
LucasArielAguirre <lucasaguirreescuela@gmail.com>
2026-06-10 22:27:51 -0300

    new change layout

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,13 +1,15 @@
 import { Instrument_Serif } from "next/font/google";
 import { Playfair_Display } from "next/font/google";
 import Link from "next/link";
+import Image from "next/image";
 
 const playfair = Playfair_Display({ subsets: ["latin"] });
 const instrumentSerif = Instrument_Serif({ subsets: ["latin"], weight: "400" });
 
 const Nav = () => {
   return (
-    <nav className="w-full h-16 flex items-center justify-end px-8 bg-blur mix-blend-color-blur border-b border-black/80">
+    <nav className="w-full h-16 flex items-center justify-between px-8 bg-blur mix-blend-color-blur border-b border-black/80">
+      <Image src="/favicon.ico" alt="Logo" width={55} height={55} />
       <ul className="flex items-center gap-4">
         <li
           className={`${playfair.className} text-xl font-extralight hover:border-b hover:border-black`}
